f5ae187aea030f70f7b88bb0e0e336a40b0aea63
gift-ndoma <giftndoma87@gmail.com>
2025-04-10 23:44:45 +0100

    initial commit

--- course-project/scripts/project.js
+++ course-project/scripts/project.js
@@ -22,5 +22,46 @@ if (toggleButton) {
 
 if (localStorage.getItem('dark-mode') === 'true') {
     document.body.classList.add('dark-mode');
+};
+
+const studyTips = [
+    {
+        tipName: "Pomodoro Technique ⏳",
+        para: "Study for 25 mins, take a 5-min break. Boost your productivity!"
+    },
+
+    {
+        tipName: "Active Recall 🧠",
+        para: "Test yourself instead of just reading – it improves memory."
+    },
+
+    {
+        tipName: "Mind Mapping 🗺️",
+        para: "Visualize concepts and make connections easily."
+    }
+];
+
+function displayTips(studyTips) {
+    const tipContainer = document.getElementById("featured-tips");
+    studyTips.forEach((tip) => {
+        let card = document.createElement("div")
+        card.classList.add("tip-card")
+        card.innerHTML = `
+        <h3>${tip.tipName}</h3>
+        <p>${tip.para}</p>
+        `
+        tipContainer.appendChild(card)
+    });
 }
 
+displayTips(studyTips);
+
+const form = document.getElementById('myForm');
+const message = document.getElementById('success-message');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    message.style.display = 'block';
+    form.reset();
+    form.querySelector('input').focus(); 
+});
